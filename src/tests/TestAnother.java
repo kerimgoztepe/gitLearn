b39844f9c6ad1b445288995dbcb927f9fd758501
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-05-05 19:30:26 +0300

    i added CreateMethod.java

---
+++ src/tests/TestAnother.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class TestAnother {
+    public static void main(String[] args) {
+        int a =3;
+        int b =4;
+        int sum = a+b;
+
+        System.out.println("sum is = "+sum);
+    }
+}
