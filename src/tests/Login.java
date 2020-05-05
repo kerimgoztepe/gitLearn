851f3d0c362b7ee6b0aebd8fc1df3b1284c2c9fb
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-05-05 19:49:31 +0300

    i added Login.java

---
+++ src/tests/Login.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class Login {
+    public static void main(String[] args) {
+        String name = "John";
+        String surName = "Berger";
+
+        System.out.println(name+" "+surName);
+    }
+
+
+}
