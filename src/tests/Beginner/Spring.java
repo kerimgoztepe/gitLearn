de359ecad489a5fd5d815baaf658d929032c28f8
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-05-06 19:28:44 +0300

    TryTest.java

--- src/tests/Beginner/Spring.java
+++ src/tests/Beginner/Spring.java
@@ -4,5 +4,7 @@ public class Spring {
     public static void main(String[] args) {
         String text1 = "Here is some text";
         System.out.println(text1);
+        //git remote add origin https://github.com/kerimgoztepe/gitLearn.git
+        //git push -u origin master
     }
 }
---
+++ src/tests/Beginner/TryTest.java
@@ -0,0 +1,13 @@
+package tests.Beginner;
+
+public class TryTest {
+    public static void main(String[] args) {
+        double x= 12.23;
+        double y= 11.11;
+        double divxy = x/y;
+
+        System.out.println("division result is ="+divxy);
+    }
+
+
+}
