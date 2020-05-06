d068f6fc205ed49ab235dab001b0774ded1a3fd4
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-05-06 18:26:11 +0300

    add Spring.java

---
+++ src/tests/Beginner/Spring.java
@@ -0,0 +1,8 @@
+package tests.Beginner;
+
+public class Spring {
+    public static void main(String[] args) {
+        String text1 = "Here is some text";
+        System.out.println(text1);
+    }
+}
